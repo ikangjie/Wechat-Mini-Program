470e0818ef50bd0b4c5f175ca40229ac73c9db47
Kangjie <1365595476@qq.com>
2019-04-19 02:55:24 +0800

    weapp-locally

---
+++ utils/fetch.js
@@ -0,0 +1,15 @@
+module.exports = (url, data, method = 'GET', header = {}) => {
+  // wx.showLoading({ title: 'Loading...' })
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: `https://locally.uieee.com${url}`,
+      data,
+      header,
+      method,
+      dataType: 'json',
+      success: resolve,
+      fail: reject,
+      complete: wx.hideLoading
+    })
+  })
+}
